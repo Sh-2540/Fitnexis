02ef3402dfd487a1462d625ab4bf19de67a2764c
Shrutika <shrutika@Shrutikas-MacBook-Air.local>
2026-06-16 09:44:40 +0530

    Tp

--- src/pages/TrackOrder.jsx
+++ src/pages/TrackOrder.jsx
@@ -1,30 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TrackOrder() {
 
+  const [phone, setPhone] = useState("");
+
+  const handleSearch = () => {
+    console.log("Phone entered:", phone);
+  };
+
   return (
+    <div style={{ paddingTop: "150px", color: "white" }}>
 
-    <div
-      style={{
-        paddingTop: "150px",
-        color: "white"
-      }}
-    >
-
-      <h1>
-        Track Order
-      </h1>
+      <h1>Track Order</h1>
 
       <input
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
         placeholder="Phone Number"
       />
 
-      <button>
+      <button onClick={handleSearch}>
         Search
       </button>
 
     </div>
-
   );
 }
 
