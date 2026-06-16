4e07611e15458b355a696892405a6635c7d88a25
Shrutika <shrutika@Shrutikas-MacBook-Air.local>
2026-06-16 15:34:29 +0530

    Tp

--- src/pages/TrackOrder.jsx
+++ src/pages/TrackOrder.jsx
@@ -39,67 +39,9 @@ function TrackOrder() {
       alert(error.message);
     }
   };
-
   return (
-    <div style={{ padding: "100px", color: "white" }}>
-
-      <h1>Track Order</h1>
-
-      <input
-        value={phone}
-        onChange={(e) => setPhone(e.target.value)}
-        placeholder="Enter phone number"
-      />
-
-      <button onClick={handleSearch}>
-        Track
-      </button>
-
-      {orders.map((order) => (
-  <div
-    key={order.id}
-    style={{
-      background: "#111",
-      color: "#fff",
-      padding: "20px",
-      marginBottom: "20px",
-      borderRadius: "12px",
-      border: "1px solid #333"
-    }}
-  >
-
-    {/* STATUS */}
-    <h3>
-      {order.status === "Processing" && "🟡 Processing"}
-      {order.status === "Packed" && "📦 Packed"}
-      {order.status === "Shipped" && "🚚 Shipped"}
-      {order.status === "Delivered" && "✅ Delivered"}
-      {!order.status && "⏳ Pending"}
-    </h3>
-
-    {/* CUSTOMER */}
-    <p>👤 Customer: {order.customer}</p>
-
-    {/* AMOUNT */}
-    <p>💰 Amount: ₹{order.total}</p>
-
-    {/* PAYMENT */}
-    <p>💳 Payment ID: {order.paymentId}</p>
-
-    {/* PRODUCTS */}
-    <h4>🛍️ Products:</h4>
-
-    {order.products?.map((product, index) => (
-      <p key={index}>
-        🧾 {product.name} × {product.qty}
-      </p>
-    ))}
-
-  </div>
-))}
-
-      
-
+    <div style={{ padding: "100px", background: "white", color: "black" }}>
+      <h1>TRACK ORDER TEST</h1>
     </div>
   );
 }
