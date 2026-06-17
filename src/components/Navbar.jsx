411f4754f87a864f497d8af1bb1125057164d3de
Shrutika <shrutika@Shrutikas-MacBook-Air.local>
2026-06-17 18:33:05 +0530

    Tp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes, FaShoppingCart } from "react-icons/fa";
-import "./Navbar.css";
+
 
 export default function Navbar({ cartCount = 0 }) {
   const [menuOpen, setMenuOpen] = useState(false);
